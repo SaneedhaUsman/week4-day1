f3fec72899e4b0651d58b140abd9c148b7a37c5d
Saneedha <usmansaneedha@gmail.com>
2026-02-25 10:51:13 +0530

    week4 day1

---
+++ day3.js
@@ -0,0 +1,11 @@
+// const person = {
+//     name : "Amina" ,
+//     age : 20 ,
+
+//     greet : function(){
+//         console.log("Hello, my name is  " + this.name);
+//     }
+// };
+
+// console.log(person.name);
+// person.greet();
